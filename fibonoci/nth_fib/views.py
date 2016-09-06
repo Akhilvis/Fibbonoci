c4f464be0e375bda41729ab821cdd1b07ff58fa4
Akhilvis <akhilviswam000@gmail.com>
2016-09-06 14:20:29 +0530

    find nth term

--- fibonoci/nth_fib/views.py
+++ fibonoci/nth_fib/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def submit_n_val(request):
+	nth_term=F(6)
+	return render(request,'result.html',{'N':nth_term})
+def Home(request):
 	return render(request,'home_page.html')
+
+def F(n):
+	a,b=1,1
+	n=6
+	for i in xrange(n-2):
+   		a,b=b,a+b
+	return b
