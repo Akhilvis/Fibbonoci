44ce4ab8a62b9eeb34c7144e5372109b09f3088c
Akhilvis <akhilviswam000@gmail.com>
2016-09-07 11:35:36 +0530

    Counter for response time calculation

--- fibonoci/nth_fib/static/js/controller.js
+++ fibonoci/nth_fib/static/js/controller.js
@@ -1,8 +1,32 @@
 var app = angular.module('FibApp', []);
-
-app.controller('fibControler', function($scope) {
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{$');
+  $interpolateProvider.endSymbol('$}');
+});
+app.controller('fibControler', function($scope,$timeout) {
     console.log("controller..")
+    $scope.countr=false;
+    $scope.countdict={}
+    $scope.countdict.count_no=0
     $scope.Aninatn=function(){
     	$('#btn1').addClass('btn-1');
+    	$timeout(function(){
+    	 $('#btn1').hide();
+    	 $scope.countr=true;
+    	 var myVar = setInterval(function(){ $scope.myTimer() }, 1000);
+    	},500)
+
     }
+$scope.myTimer=function() {
+	console.log("mytimer....")
+   $scope.countdict.count_no++;
+   document.getElementById("counter").innerHTML = $scope.countdict.count_no;
+
+}
+
+$scope.myStopFunction=function() {
+    clearInterval(myVar);
+}
+
+
 });
