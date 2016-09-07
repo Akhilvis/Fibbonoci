89f377e5315cb1e1da8023e8012399d137735bfd
Akhil Viswam <akhilviswam000@gmail.com>
2016-09-07 23:17:11 +0530

    Exception handle

--- fibonoci/nth_fib/static/js/controller.js
+++ fibonoci/nth_fib/static/js/controller.js
@@ -19,14 +19,8 @@ app.controller('fibControler', function($scope,$timeout,$http) {
   $scope.send=function(){
   	if($scope.val>0&&$scope.val!=undefined){
 
-		$('#btn1').addClass('btn-1');
-    	$timeout(function(){
-    	 $('#btn1').hide();
-    	 $('#txtbox').hide();
-    	},500)
-
-
   	    	$scope.myVar = setInterval(function(){ $scope.myTimer() }, 1);
+  	    	$scope.alert=""
   	    	console.log($scope.val)
  			$http({
                     method  : 'POST',
@@ -34,15 +28,28 @@ app.controller('fibControler', function($scope,$timeout,$http) {
                     data    : {"n":$scope.val}, // pass in data as strings
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'} // set the headers so angular passing info as form data (not request payload)
                     }).success(function(data) {
+
                     	$scope.nth_term=data['N']
+                    	if(isFinite($scope.nth_term))
+                    	{
+                    	$scope.myStopFunction()
+    		        	$('#btn1').addClass('btn-1');
+				    	$timeout(function(){
+				    	 $('#btn1').hide();
+				    	 $('#txtbox').hide();
+				    	},0)
                     	$timeout(function(){
                     	$scope.rslt_stat=true
                     	},500)
                 		console.log("myStopFunction...")
-                    	$scope.myStopFunction()
+                    	}
+                    	else{
+                    		$scope.alert="Result is too big to display!"
+                    		$scope.retry()
+                    	}
                     })
   }
-  else if($scope.val<0){$scope.alert="Please enter positive values!"}
+ 	  else if($scope.val<0){$scope.alert="Please enter positive values!"}
   	  else {$scope.alert="Please enter a value!"}
 
 }
@@ -53,6 +60,13 @@ $scope.myTimer=function() {
 $scope.myStopFunction=function() {
     clearInterval($scope.myVar);
 }
-
+$scope.retry=function(){
+	$scope.rslt_stat=false
+	$scope.val=""
+    $scope.nth_term=""
+    $scope.countdict.count_no=0
+    $('#btn1').show();
+ 	$('#txtbox').show();
+}
 
 });
